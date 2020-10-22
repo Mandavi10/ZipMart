1c1ddd3f2b7a4cae0a942ce789cfa14a9bef414f
TARUN SHARMA <rtdigitalsolution@gmail.com>
2020-10-22 17:14:05 -0500

    gggd

--- src/app/Master/dashboard/dashboard.component.ts
+++ src/app/Master/dashboard/dashboard.component.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  name = 'Angular';
+  simpleAlert(){
+    Swal.fire('Hello world!');
+  }
+  
+  alertWithSuccess(){
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success')
+  }
+  
+  confirmBox(){
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+  }
   dropdownList = [];
   selectedItems = [];
    dropdownSettings:IDropdownSettings;
