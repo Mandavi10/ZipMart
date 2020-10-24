c3e45019e96e30c03ebf8ae9b1074c4fccb71ace
TARUN SHARMA <rtdigitalsolution@gmail.com>
2020-10-24 16:14:03 -0500

    jjj

--- src/app/Master/dashboard/dashboard.component.ts
+++ src/app/Master/dashboard/dashboard.component.ts
@@ -12,6 +12,8 @@ export class DashboardComponent implements OnInit {
   public minDate: Date = new Date ("05/07/2017");
   public maxDate: Date = new Date ("05/27/2017");
   public dateValue: Date = new Date ("05/16/2017");
+  public headerText: Object = [{ text: "Twitter", 'iconCss': 'e-twitter' },
+  { text: "Facebook", 'iconCss': 'e-facebook' }, { text: "WhatsApp", 'iconCss': 'e-whatsapp' }];
   name = 'Angular';
   simpleAlert(){
     Swal.fire('Hello world!');
